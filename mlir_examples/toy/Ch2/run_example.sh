60cfaf4feb0073577e378f1127dbd39d7d56414d
Hyunjin Kim <roykim0823@gmail.com>
2026-02-12 17:52:17 +0900

    fix: cmakefile in mlir ch2
    Signed-off-by: Hyunjin Kim <roykim0823@gmail.com>

--- mlir_examples/toy/Ch2/run_example.sh
+++ mlir_examples/toy/Ch2/run_example.sh
@@ -1,2 +1,2 @@
-mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o dialect-decls.inc 
-mlir-tblgen -gen-op-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o op-defs.inc 
+mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o ./build/dialect-decls.inc 
+mlir-tblgen -gen-op-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o ./build/op-defs.inc 
