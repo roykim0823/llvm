5d24e1acaa32423c34e2bab6e47ce6dca3761a8f
hj-kim-dn <162398262+hj-kim-dn@users.noreply.github.com>
2026-02-13 15:30:40 +0900

    add: update llvm_examples in llvm21.1.8 version except for the Refactor dirs
    Signed-off-by: hj-kim-dn <162398262+hj-kim-dn@users.noreply.github.com>

--- llvm_examples/Kaleidoscope/Chapter8/toy.cpp
+++ llvm_examples/Kaleidoscope/Chapter8/toy.cpp
@@ -1218,14 +1218,17 @@ int main() {
   MainLoop();
 
   // Initialize the target registry etc.
-  InitializeAllTargetInfos();
-  InitializeAllTargets();
-  InitializeAllTargetMCs();
-  InitializeAllAsmParsers();
-  InitializeAllAsmPrinters();
+  //InitializeAllTargetInfos();
+  //InitializeAllTargets();
+  //InitializeAllTargetMCs();
+  //InitializeAllAsmParsers();
+  //InitializeAllAsmPrinters();
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
 
   auto TargetTriple = sys::getDefaultTargetTriple();
-  TheModule->setTargetTriple(TargetTriple);
+  TheModule->setTargetTriple(Triple(TargetTriple));
 
   std::string Error;
   auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
@@ -1242,9 +1245,8 @@ int main() {
   auto Features = "";
 
   TargetOptions opt;
-  auto RM = std::optional<Reloc::Model>();
-  auto TheTargetMachine =
-      Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+  auto TheTargetMachine = Target->createTargetMachine(
+      Triple(TargetTriple), CPU, Features, opt, Reloc::PIC_);
 
   TheModule->setDataLayout(TheTargetMachine->createDataLayout());
 
@@ -1258,7 +1260,7 @@ int main() {
   }
 
   legacy::PassManager pass;
-  auto FileType = CGFT_ObjectFile;
+  auto FileType = CodeGenFileType::ObjectFile;
 
   if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
     errs() << "TheTargetMachine can't emit a file of this type";
