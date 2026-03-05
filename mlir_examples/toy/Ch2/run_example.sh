8703a9de344b8c1b8ee2aae361da11d451cda834
Hyunjin Kim <roykim0823@gmail.com>
2026-03-05 16:56:59 +0900

    fix: fix problems up to toy/Ch2
    Signed-off-by: Hyunjin Kim <roykim0823@gmail.com>

--- mlir_examples/toy/Ch2/run_example.sh
+++ mlir_examples/toy/Ch2/run_example.sh
@@ -1,2 +1,7 @@
-mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o ./build/dialect-decls.inc 
-mlir-tblgen -gen-op-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm/include/ -o ./build/op-defs.inc 
+mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/dialect-decls.inc 
+mlir-tblgen -gen-op-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/op-defs.inc
+./build/toyc-ch2 codegen.toy -emit=mlir -mlir-print-debuginfo
+
+# Do the RoundTrip:
+./build/toyc-ch2 codegen.toy -emit=mlir -mlir-print-debuginfo 2> ./build/codegen.mlir
+./build/toyc-ch2 ./build/codegen.mlir -emit=mlir
