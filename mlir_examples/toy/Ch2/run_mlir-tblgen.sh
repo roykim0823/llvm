3771521087c177251878ed41e67ced09f8a7cfe6
hj-kim-dn <162398262+hj-kim-dn@users.noreply.github.com>
2026-03-26 17:56:29 +0900

    misc: change the run_mlir-tblgen.sh in Ch2
    Signed-off-by: hj-kim-dn <162398262+hj-kim-dn@users.noreply.github.com>

--- mlir_examples/toy/Ch2/run_mlir-tblgen.sh
+++ mlir_examples/toy/Ch2/run_mlir-tblgen.sh
@@ -1,3 +1,4 @@
-mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/dialect-decls.inc 
+mlir-tblgen -gen-dialect-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/dialect-decls.inc
+mlir-tblgen -gen-dialect-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/dialect-defs.inc
 mlir-tblgen -gen-op-decls ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/op-decls.inc
 mlir-tblgen -gen-op-defs ./include/toy/Ops.td -I /opt/homebrew/opt/llvm@20/include/ -o ./build/op-defs.inc
